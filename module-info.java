e11bec8055c30ce5b33b95e3016f15c21245bf9a
or3005 <91917236+or3005@users.noreply.github.com>
2024-05-07 23:56:56 +0300

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.algorithm {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.algorithm to javafx.fxml;
-    exports com.example.algorithm;
-}
